3a536f5d2962c440e5cf9ff0056990671263c4ce
milinda-redberyl <milinda@redberylit.com>
2023-09-14 22:00:21 +0530

    initial commit

---
+++ components/_App/LeftSidebar/SidebarData.js
@@ -0,0 +1,142 @@
+import React from "react";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import GridViewIcon from "@mui/icons-material/GridView";
+import LayersIcon from "@mui/icons-material/Layers";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import LockIcon from "@mui/icons-material/Lock";
+import SettingsIcon from "@mui/icons-material/Settings";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import AddchartIcon from "@mui/icons-material/Addchart";
+import CopyAllIcon from "@mui/icons-material/CopyAll";
+import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
+import ViewQuiltIcon from "@mui/icons-material/ViewQuilt";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+
+export const SidebarData = [
+  {
+    title: "Dashboard",
+    path: "/",
+    icon: <GridViewIcon />,
+  },    
+  {
+    title: "Gas stations",
+    path: "/contact-list/",
+    icon: <PostAddIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+
+    subNav: [      
+      {
+        title: "Members List",
+        path: "/contact-list/members-list/",
+      },
+      {
+        title: "Profile",
+        path: "/contact-list/profile/",
+      },
+    ],
+  },
+  {
+    title: "Customers",
+    path: "/projects/",
+    icon: <CopyAllIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+
+    subNav: [
+      {
+        title: "Projects",
+        path: "/projects/",
+      },      
+    ],
+  },
+  {
+    title: "Products",
+    path: "/ecommerce/products/",
+    icon: <ShoppingCartCheckoutIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+
+    subNav: [
+      {
+        title: "Products",
+        path: "/ecommerce/products/",
+      },
+      {
+        title: "Product Details",
+        path: "/ecommerce/product-details/",
+      },
+      {
+        title: "Create Product",
+        path: "/ecommerce/create-product/",
+      },
+      {
+        title: "Orders List",
+        path: "/ecommerce/orders-list/",
+      },
+      {
+        title: "Order Details",
+        path: "/ecommerce/order-details/",
+      },
+      {
+        title: "Customers",
+        path: "/ecommerce/customers/",
+      },
+    ],
+  }, 
+  {
+    title: "Authentication",
+    path: "/authentication/sign-in/",
+    icon: <LockIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+
+    subNav: [
+      {
+        title: "Sign Up",
+        path: "/authentication/sign-up/",
+      },
+      {
+        title: "Forgot Password",
+        path: "/authentication/forgot-password/",
+      },
+      {
+        title: "Lock Screen",
+        path: "/authentication/lock-screen/",
+      },
+      {
+        title: "Logout",
+        path: "/authentication/logout/",
+      },
+    ],
+  },
+  {
+    title: "Settings",
+    path: "/settings/account/",
+    icon: <SettingsIcon />,
+    iconClosed: <KeyboardArrowRightIcon />,
+    iconOpened: <KeyboardArrowDownIcon />,
+
+    subNav: [
+      {
+        title: "Account",
+        path: "/settings/account/",
+      },
+      {
+        title: "Security",
+        path: "/settings/security/",
+      },
+      {
+        title: "Terms & Conditions",
+        path: "/pages/terms-conditions/",
+      },
+      {
+        title: "Logout",
+        path: "/authentication/logout/",
+      },
+    ],
+  },
+];
